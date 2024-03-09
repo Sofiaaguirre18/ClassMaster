12adb84702f09b52a046e4d46bc814545e086001
Sofia Aguirre <princesssofia.aguirre@unc.edu.ph>
2024-03-09 08:26:04 +0800

    pia

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,95 +1,110 @@
 <template>
-    <v-card>
-      <v-layout>
-        <v-navigation-drawer
-          v-model="drawer"
-          :rail="rail"
-          permanent
-          app
-          @click="rail = false"
-          color="primary"
-          class="gradient-bg"
-        >
-          <v-list-item
-            prepend-avatar="~@/assets/UNC-logo.png"
-            title="CLASS MASTER"
-            class="title-text-color"
-            nav
-          >
-          </v-list-item>
-  
-          <v-divider></v-divider>
-  
-          <v-list dense nav>
-            <v-list-item
-              v-for="(item, index) in items"
-              :key="index"
-              :prepend-icon="item.icon"
-              :title="item.title"
-              :value="item.value"
-              class="buttons-text-color"
-            ></v-list-item>
-          </v-list>
-  
-          <template v-slot:append>
-            <v-btn
-              icon="mdi-chevron-left"
-              variant="text"
-              @click.stop="rail = !rail"
-            ></v-btn>
-          </template>
-  
-        </v-navigation-drawer>
-        <v-main style="height: 750px"></v-main>
-      </v-layout>
-    </v-card>
-  </template>
-  
-  <script>
-  export default {
-    data () {
-      return {
-        drawer: true,
-        rail: true,
-        items: [
-          { icon: 'mdi-view-dashboard', title: 'Home', value: 'home' },
-          { icon: 'mdi-cached', title: 'Cohort Loading', value: 'cohortLoading' },
-          { icon: 'mdi-calendar', title: 'Schedule', value: 'schedule' },
-          { icon: 'mdi-account', title: 'Student', value: 'student' },
-          { icon: 'mdi-account-group-outline', title: 'Faculty', value: 'faculty' },
-          { icon: 'mdi-widgets', title: 'Section', value: 'section' }
-        ]
-      }
-    },
-  }
-  </script>
-  
-  <style scoped>
-  .gradient-bg {
-    background: linear-gradient(to bottom , #F78C8C, #9B9B9B); 
-  }
-  
-  .custom-text-color:hover {
-    background-color: #ffadb1;
-    position: relative;
-  }
-  
-  .custom-text-color:hover::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    width: 3px; 
-    background-color: #EF5050;
-  }
-  
-  .buttons-text-color {
-    color: #010101; 
-  }
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        app
+        @click="rail = false"
+        color="primary"
+        class="gradient-bg"
+      >
+        <v-list-item
+          prepend-avatar="~@/assets/UNC-logo.png"
+          title="CLASS MASTER"
+          class="title-text-color"
+          nav
+        ></v-list-item>
 
-  .title-text-color {
-    color: #010101; 
-  }
-  </style>
-  
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :prepend-icon="item.icon"
+            :title="item.title"
+            :value="item.value"
+            class="buttons-text-color"
+          ></v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <v-btn
+            icon
+            @click.stop="rail = !rail"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </template>
+      </v-navigation-drawer>
+      <v-main style="height: 750px">
+       
+        
+
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex xs12 sm6>
+              <v-card class="home-card">
+                <v-card-title class="title-text-color">
+                  Welcome to Class Master Dashboard!
+                </v-card-title>
+                <v-card-text>
+                  <p>
+                    kskskkss
+                  </p>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-card class="home-card">
+                <v-card-title class="title-text-color">
+                  Today's Schedule
+                </v-card-title>
+                <v-card-text>
+                  <p>
+                    kskkskkss
+                  </p>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      rail: true,
+      items: [
+        { icon: 'mdi-view-dashboard', title: 'Home', value: 'home' },
+        { icon: 'mdi-cached', title: 'Cohort Loading', value: 'cohortLoading' },
+        { icon: 'mdi-calendar', title: 'Schedule', value: 'schedule' },
+        { icon: 'mdi-account', title: 'Student', value: 'student' },
+        { icon: 'mdi-account-group-outline', title: 'Faculty', value: 'faculty' },
+        { icon: 'mdi-widgets', title: 'Section', value: 'section' }
+      ]
+    };
+  },
+};
+</script>
+
+<style scoped>
+.gradient-bg {
+  background: linear-gradient(to bottom, #f78c8c, #9b9b9b);
+}
+
+.home-card {
+  margin: 20px;
+}
+
+.title-text-color {
+  color: #010101;
+}
+</style>
